ee8e1f6f391e9ca5e09216150ebb187178a441fd
James Kozianski <soulplant@gmail.com>
2011-02-05 08:40:27 +1100

    Removed unused imports from li.py.

--- workspaces/li.py
+++ workspaces/li.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 
-import os
-import glob
 import optparse
 from workspace import Workspace
 
 (options, args) = optparse.OptionParser().parse_args()
 
 if len(args) == 0:
-    # list available workspaces
     ws = Workspace.all()
     for i in range(len(ws)):
         print "%d: %s" % (i, ws[i].summary_line())
