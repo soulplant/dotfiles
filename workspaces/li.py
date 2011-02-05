dada15934bdc90aef4af06a242f9d74f881ea004
James Kozianski <soulplant@gmail.com>
2011-02-06 08:08:22 +1100

    Restructured li so it takes the name of the command to execute.

--- workspaces/li.py
+++ workspaces/li.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python
 
 from wsprompt import WSPrompt
-import optparse
+import sys
 
-(options, args) = optparse.OptionParser().parse_args()
+class Command(object):
+    def name(self):
+        return self._name
 
-ws = WSPrompt(args).run()
+    def run(self, ws, args):
+        pass
 
-if ws:
-    ws.open()
+class OpenCommand(Command):
+    def __init__(self):
+        self._name = "open"
+
+    def run(self, ws, args):
+        ws.open()
+
+class AddNoteCommand(Command):
+    def __init__(self):
+        self._name = "add-note"
+
+    def run(self, ws, args):
+        if len(args) > 1:
+            ws.add_note(args[1])
+        else:
+            print "Usage: %s workspace-index note"
+
+# First is default.
+COMMANDS = [OpenCommand, AddNoteCommand]
+
+WSPrompt(sys.argv[1:], [c() for c in COMMANDS]).run()
--- workspaces/wsprompt.py
+++ workspaces/wsprompt.py
@@ -3,18 +3,31 @@
 import optparse
 from workspace import Workspace
 
-(options, args) = optparse.OptionParser().parse_args()
-
 class WSPrompt(object):
-    def __init__(self, args):
+    def __init__(self, args, commands):
         self._args = args
+        self._commands = {}
+        for c in commands:
+            self._commands[c.name()] = c
+        self._default_command = commands[0]
 
     def run(self):
-        if len(args) == 0:
+        (ws, args) = self.prompt_for_workspace()
+        if ws is None:
+            pass
+        elif len(args) == 0:
+            self._default_command.run(ws, args)
+        elif args[0] in self._commands:
+            self._commands[args[0]].run(ws, args)
+        else:
+            print "Unknown command: %s" % args[0]
+
+    def prompt_for_workspace(self):
+        if len(self._args) == 0:
             ws = Workspace.all()
             for i in range(len(ws)):
                 print "%d: %s" % (i, ws[i].summary_line())
-            return None
+            return (None, [])
         else:
-            i = int(args[0])
-            return Workspace.all()[i]
+            i = int(self._args[0])
+            return (Workspace.all()[i], self._args[1:])
